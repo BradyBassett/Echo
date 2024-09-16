8ba5fa66aeb1f23afd81919e2eafd7eca1e79d38
BradyBassett <bradydbassett@gmail.com>
2024-09-16 15:44:40 -0500

    Normalize

--- database/insert_friends.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE friends (
-	user_id1 int REFERENCES users(id) ON DELETE CASCADE,
-	user_id2 int REFERENCES users(id) ON DELETE CASCADE,
-	status VARCHAR(20) DEFAULT 'pending' NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	PRIMARY KEY (user_id1, user_id2)
-);
--- database/insert_notifications.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE notifications (
-	id SERIAL PRIMARY KEY,
-	user_id INT REFERENCES users(id) ON DELETE CASCADE,
-	type VARCHAR(50) NOT NULL,
-	message_id INT REFERENCES messages(id) ON DELETE CASCADE,
-	content_preview TEXT NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
