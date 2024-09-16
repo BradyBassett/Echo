32ee73193f6d3246d650bd2f00a6e007f2f75fdc
BradyBassett <bradydbassett@gmail.com>
2024-09-16 15:43:10 -0500

    Update

--- database/insert_channel_members.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE channel_members (
-	channel_id INT REFERENCES channels(id) ON DELETE CASCADE,
-	user_id INT REFERENCES users(id) ON DELETE CASCADE,
-	joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	PRIMARY KEY (channel_id, user_id)
-);
--- database/insert_read_recipts.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE read_recipts (
-	message_id INT REFERENCES messages(id) ON DELETE CASCADE,
-	user_id INT REFERENCES users(id),
-	read_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	PRIMARY KEY (message_id, user_id)
-);
